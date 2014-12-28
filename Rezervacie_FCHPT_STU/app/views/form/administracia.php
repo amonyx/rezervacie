b2c5aa65794916ee318af397ec201388fad929ad
JanJohny Minarciny <minarciny.j@gmail.com>
2014-12-28 16:24:19 +0100

    quickfix administracia.php
    quickfix administracia.php

--- Rezervacie_FCHPT_STU/app/views/form/administracia.php
+++ Rezervacie_FCHPT_STU/app/views/form/administracia.php
@@ -2,8 +2,7 @@
 //******************ZMENA
 	if($this->user != null){				
 		if($this->user->admin){								
-			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/CreateNewUser">Vytvorenie noveho pouzivatela</a></li>';					
-			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/CreateNewUser">Vytvorenie novej miestnosti</a></li>';					
+			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/CreateNewUser">Vytvorenie noveho pouzivatela</a></li>';							
 			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/ZmenaPrav">Zmena administratoskych prav</a></li>';
 			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/CreateNewRoom">Vytvorenie novej miestnosti</a></li>';
 			echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin/handleRooms">Správa miestností</a></li>';
