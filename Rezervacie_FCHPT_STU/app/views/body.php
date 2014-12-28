b6635308994f5e45d60a1bed3de4674de33c6169
Peter Gubik <p.gubik@gmail.com>
2014-12-28 16:18:56 +0100

    hotfix
    opravená verzia init, config, body

--- Rezervacie_FCHPT_STU/app/views/body.php
+++ Rezervacie_FCHPT_STU/app/views/body.php
@@ -1,34 +1,35 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title><?=$title?></title>
-	<link rel="stylesheet" type="text/css" href="http://<?='DOMAIN'?>/<?='URL_BASE'?>/css/bootstrap.css">
+	<link rel="stylesheet" type="text/css" href="http://<?=DOMAIN?>/<?=URL_BASE?>/css/bootstrap.css">
 </head>
 <body>
 	<header>
 		<?php 
 			if($this->user == null){
 				?>
-				<a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Ucitel/prihlasenie">Prihl·siù</a>
+				<a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Ucitel/prihlasenie">Prihl√°si≈•</a>
 				<?php 
 			}
 			else{
 				?>
-				<a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Ucitel/odhlasenie">Odhl·siù</a>
+				<a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Ucitel/odhlasenie">Odhl√°si≈•</a>
 				<?php 
 			}
 		?>
 	</header>
 	<nav>
 		<ul>
-			<li><a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Vyhladavanie">Vyhæad·vanie</a></li>
+			<li><a href="http://<?=DOMAIN?>/<?=URL_BASE?>/Vyhladavanie">Vyhƒæad√°vanie</a></li>
 		<?php 
 			if($this->user != null){
-				echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Ucitel/kalendar">Kalend·r</a></li>';
-				echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Ucitel/rezervacia">Nov· rezerv·cia</a></li>';
+				echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Ucitel/kalendar">Kalend√°r</a></li>';
+				echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Ucitel/rezervacia">Nov√° rezerv√°cia</a></li>';
 				echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Ucitel/zmenaHesla">Zmena Hesla</a></li>';
 				if($this->user->admin){
-					echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin">Administr·cia</a></li>';										
+					echo '<li><a href="http://'.DOMAIN.'/'.URL_BASE.'/Admin">Administr√°cia</a></li>';										
 				}
 			}
 		?>
