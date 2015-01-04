ad5025051bc0722ecc2442d02929b0b8b1958df0
Peter Gubik <p.gubik@gmail.com>
2015-01-04 18:20:24 +0100

    upravený rezervačný formulár
    zatiaľ bez funkcionality

--- Rezervacie_FCHPT_STU/app/views/form/rezervacia.php
+++ Rezervacie_FCHPT_STU/app/views/form/rezervacia.php
@@ -1,146 +1,138 @@
 <?php 
-//******************ZMENA
-     function showDateSelector($text) {
-		$currentDay = date("j");
-		$currentMonth = date("n");
-		$currentYear = date("Y");
-		echo '<select name="' . $text . 'Day' . '">';
-		for ($i=1; $i<=31; $i++){
-			if ($currentDay == $i) {
-				echo '<option value="' . $i . '" selected="selected">' . $i . '</option>' . "\n";
+
+function showTimeSelector($text){
+	echo ' <select name="' . $text . 'Hour">';
+	for ($i = 0; $i <= 23; $i++) {
+		if ($i<10) {
+			if ($i==9 && $text=="start") {
+			echo '<option value="0' . $i . '" selected="selected">0' . $i . '</option>' . "\n";
 			}
-			else {
-				echo '<option value="' . $i . '">' . $i . '</option>' . "\n";
-			}
+		else {	
+		  echo '<option value="0' . $i . '">0' . $i . '</option>' . "\n";
+		  }
 		}
-		echo '</select>';
-		
-		$months = array("Január", "Február", "Marec", "Apríl", "Máj", "Jún", "Júl", "August", "September", "Október", "November", "December");
-		echo '<select name="' . $text . 'Month' . '">';
-		for ($i=0; $i<12; $i++){
-			if ($currentMonth == ($i+1)) {
-				echo '<option value="' . $months[$i] . '" selected="selected">' . $months[$i] . '</option>' . "\n";
+		else {
+			if ($text=="end" && $i==11) {
+			echo '<option value="' . $i . '" selected="selected">' . $i . '</option>' . "\n";
 			}
-			else {
-				echo '<option value="' . $months[$i] . '">' . $months[$i] . '</option>' . "\n";
-			}
+		  echo '<option value="' . $i . '">' . $i . '</option>' . "\n";
 		}
-		echo '</select>';
-		
-		echo '<select name="' . $text . 'Year' . '">';
-		for ($i=2014; $i<=2030; $i++){
-			if ($currentYear == ($i+1)) {
-				echo '<option value="' . $i . '" selected="selected">' . $i . '</option>' . "\n";
-			}
-			else {
-				echo '<option value="' . $i . '">' . $i . '</option>' . "\n";
-			}
-		}	
-		echo '</select>';
-		
 	}
+	echo '</select> : ';
+	
+	echo ' <select name="' . $text . 'Minute">';
+	for ($i = 0; $i < 12; $i++) {
+		if ($i<2) {
+		  echo '<option value="0' . $i*5 . '">0' . $i*5 . '</option>' . "\n";
+		}
+		else {
+		  echo '<option value="' . $i*5 . '">' . $i*5 . '</option>' . "\n";
+		}
+	}
+	echo '</select>';
+}
+
 	
 	if($this->user != null){	
-
+		
 		?>
 		
-		<h3>Pridanie rezervacie</h3>
+		<h3>Pridanie rezervácie</h3>
 		
 		<form method="post">
-		Typy miestnosti: <br>
-		<input type="checkbox" name="lab" id="lab"> <label for="lab">Laboratorium</label><br>
-		<input type="checkbox" name="kanc" id="kanc"> <label for="kanc">Kancelaria</label><br>
-		<input type="checkbox" name="ucebna" id="ucebna"> <label for="ucebna">Ucebna</label><br>
+		Typy miestností: <br>
+		<?php 
+			$mysql = new Connection();
+			$result = $mysql->getRoomTypes();		
+			for($i=0; $i < count($result); $i++){
+					echo '<input type="checkbox" name="' . $result[$i]['ID'] . '" id="room' . $result[$i]['ID'] . '">';
+					echo '<label for="room' . $result[$i]['ID'] . '"> ' . $result[$i]['Nazov'] . '</label><br>';							
+			} 
+		?>
 		
 		<p>
-		<label for="miestnost">Vyber miestnosti</label>
+		<label for="miestnost">Výber miestnosti: </label>
 		<select name="miestnost" id="miestnost">
-			<option value="room1">Miestnost 1</option>
-			<option value="room2">Miestnost 2</option>
-			<option value="room3">Miestnost 3</option>
+		<?php 
+			$mysql = new Connection();
+			$result = $mysql->getAllRooms();		
+			for($i=0; $i < count($result); $i++){
+					echo '<option value="' . $result[$i]['Nazov'] . '">' . $result[$i]['Nazov'] . '</option>' . "\n";
+			} 
+		?>	
 		</select></p>
 		
-		<p><label for="ucel">Ucel rezervacie: </label><br>
-		<textarea name="ucel" id="ucel" rows="6" cols="25" placeholder="Vyplnte ucel vytvorenia rezervacie"></textarea></p>
+		<p><label for="ucel">Účel rezervácie: </label><br>
+		<textarea name="ucel" id="ucel" rows="6" cols="25" placeholder="Vyplňte účel vytvorenia rezervácie"></textarea></p>
 		
-		<p><label for="pocetOsob">Pocet osob: </label>
-		<input type="range" name="pocetOsob" oninput="number1.value=pocetOsob.value" id="pocetOsob" min="1" max="20">
-		<output name="number1" for="pocetOsob">10</output></p>
+		<p><label for="pocetOsob">Počet osôb: </label>
+		<input type="range" name="pocetOsob" oninput="number1.value=pocetOsob.value" id="pocetOsob" min="1" max="20" value="10">
+		<output name="number1" for="pocetOsob" id="number1">10</output></p>
+		<!-- <div id="demo"></div> -->
+
+		<script type="text/javascript">
+
+		var miestnosti_php = <?php 
+			$mysql = new Connection(); 
+			$result = $mysql->getAllRooms();		
+			$result_string = "";
+			for($i=0; $i < count($result); $i++){
+				$result_string = $result_string . $result[$i]['Nazov']. ',' . $result[$i]['Kapacita'] . '#';
+			}
+			echo json_encode($result_string);
+		?>;
+
+		var pole_miestnosti = miestnosti_php.split("#");
+		//document.getElementById("demo").innerHTML = pole_miestnosti;
+		</script>
+		<script type="text/javascript">
+		$(document).ready(function() {
+		    $("#miestnost").change(function() {
+		       var room = $("#miestnost").val();
+		       var ind = pole_miestnosti.indexOf(room);
+
+		       $("#pocetOsob").attr({
+			      "value" : pole_miestnosti[ind-1]/2,
+			      "max" : pole_miestnosti[ind-1]
+			    });
+		       $("#number1").html(pole_miestnosti[ind-1]/2);
+		    }); 
+		});
+		</script>
 		
 		<p>
-		Od kedy:
-		
+		Začiatok:
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
+	    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+	    <script src="../js/jquery-ui.js"></script>
+		<script>
+		  $(function() {
+		    $("#datepicker").datepicker();
+		  });
+  		</script>
+		<input type="text" name="startDate" id="datepicker">
 		<?php
-		showDateSelector("start");
-		echo ' <select name="startHour">';
-		for ($i = 0; $i <= 23; $i++) {
-			if ($i<10) {
-				if ($i==9) {
-				echo '<option value="0' . $i . '" selected="selected">0' . $i . '</option>' . "\n";
-				}
-			else {	
-			  echo '<option value="0' . $i . '">0' . $i . '</option>' . "\n";
-			  }
-			}
-			else {
-			  echo '<option value="' . $i . '">' . $i . '</option>' . "\n";
-			}
-		}
-		echo '</select> : ';
-		
-		echo '<select name="endHour">';
-		for ($i = 0; $i < 12; $i++) {
-			if ($i<2) {
-			  echo '<option value="0' . $i*5 . '">0' . $i*5 . '</option>' . "\n";
-			}
-			else {
-			  echo '<option value="' . $i*5 . '">' . $i*5 . '</option>' . "\n";
-			}
-		}
-		echo '</select>';
+		showTimeSelector("start");
+		?>
+		</p>
+
+		<p>
+		Koniec:
+		<?php
+		showTimeSelector("end");
 		?>
 		</p>
 		
-		<p>
-		Do kedy:
-		<?php
-		showDateSelector("end");
-		echo ' <select name="endHour">';
-		for ($i = 0; $i <= 23; $i++) {
-			if ($i<10) {
-			  echo '<option value="0' . $i . '">0' . $i . '</option>' . "\n";
-			}
-			else {
-			if ($i==11){
-				echo '<option value="' . $i . '" selected="selected">' . $i . '</option>' . "\n";
-			} else {
-			  echo '<option value="' . $i . '">' . $i . '</option>' . "\n";
-			}
-			}
-		}
-		echo '</select> : ';
-		
-		echo '<select name="endMinute">';
-		for ($i = 0; $i < 12; $i++) {
-			if ($i<2) {
-			  echo '<option value="0' . $i*5 . '">0' . $i*5 . '</option>' . "\n";
-			}
-			else {
-			  echo '<option value="' . $i*5 . '">' . $i*5 . '</option>' . "\n";
-			}
-		}
-		echo '</select>';
-		?>
-		</p>
-		
-		<p><label for="opakovania">Pocet opakovani: </label>
+		<p><label for="opakovania">Počet opakovaní: </label>
 		<input type="range" name="opakovania" oninput="number2.value=opakovania.value" id="opakovania" min="1" max="10" value="1">
 		<output name="number2" for="opakovania">1</output></p>
 		
-		<input type="button" value="Pridat">
+		<input type="submit" name="vytvorRezervaciu" value="Vytvoriť rezerváciu">
 		
 		</form>
 		<?php
+		
 	}
-//******************KONIEC ZMENY
+
+
 ?>
